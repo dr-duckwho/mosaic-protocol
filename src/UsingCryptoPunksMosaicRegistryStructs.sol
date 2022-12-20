b36b735f019a6d6c2ff88e5e50c288a05569528e
JYTK <jyterencekim@gmail.com>
2022-12-20 23:04:28 +0900

    MOSAIC-14 Add id and mono count related fields under Original for governance support

--- src/UsingCryptoPunksMosaicRegistryStructs.sol
+++ src/UsingCryptoPunksMosaicRegistryStructs.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.17;
 
 interface UsingCryptoPunksMosaicRegistryStructs {
     struct Original {
+        uint192 id;
         uint256 punkId;
+        /**
+         * @dev To calculate governance quorum and token circulation. 
+         *      Corresponds to total ticket circulation per group.
+         */
+        uint128 totalMonoCount;
+        uint128 claimedMonoCount;
     }
 }
