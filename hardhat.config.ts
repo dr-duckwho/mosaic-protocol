76f0ece089700776e97aa9e373c0a2d546efb368
kimhodol <woonjangahn@gmail.com>
2023-01-06 16:32:10 +0900

    Integrate hardhat framework

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import * as fs from "fs";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-preprocessor";
+
+const getRemappings = () => fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY ?? ""]
+    }
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+};
+
+export default config;
