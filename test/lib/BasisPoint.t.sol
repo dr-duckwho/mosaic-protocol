54a5dcf5114f85be5223c3d84c704da44a7a2658
JYTK <jyterencekim@gmail.com>
2023-03-01 19:05:49 +0900

    Add a basic unit test for BasisPoint

---
+++ test/lib/BasisPoint.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+import {TestUtils} from "../TestUtils.sol";
+
+import "../../src/lib/BasisPoint.sol";
+
+contract BasisPointTest is Test, TestUtils {
+    function test_calculateBasisPoint() public {
+        // given
+        uint256 amount = 10000;
+        uint256 bps = 37 * 100; // 37%
+
+        // then
+        uint256 result = BasisPoint.calculateBasisPoint(amount, bps);
+        assertEq(result, 3700);
+    }
+}
